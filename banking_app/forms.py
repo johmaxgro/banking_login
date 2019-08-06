ee756ccb90a5066eb41e2415255e283f066031f1
Johannes Groschopp <johannes.groschopp@gymder.de>
2019-08-06 14:33:30 +0200

    added registration page and form

--- banking_app/forms.py
+++ banking_app/forms.py
@@ -7,3 +7,16 @@ class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+	name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=20)])
+	surname = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_email(self, email):
+		user_mail = User.query.filter_by(email=email.data).first()
+		if user_mail:
+			raise ValidationError('This email is already taken. Please use a different address.')
--- banking_app/routes.py
+++ banking_app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from banking_app import app, db, bcrypt
 from banking_app.models import User
-from banking_app.forms import LoginForm
+from banking_app.forms import LoginForm, RegisterForm
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -12,6 +12,20 @@ def main():
 	if not current_user.is_authenticated:
 		return redirect(url_for('login'))
 	return render_template('main.html', title='Your Online Banking App', header='Your Personal Account')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('main'))
+	form = RegisterForm()
+	if form.validate_on_submit():
+		hash_pw = bcrypt.generate_password_hash(form.password.data).decode('UTF-8')
+		user = User(name=form.name.data, surname=form.surname.data, email=form.email.data, password=hash_pw)
+		db.session.add(user)
+		db.session.commit()
+		flash('You are now registered! Log in to reach to Account', 'success')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Your Online Banking App', header='Register Your Online Banking Account', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
