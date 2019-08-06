199afdeb6c88cbad5f89849233432f2d6b4d1292
Johannes Groschopp <johannes.groschopp@gymder.de>
2019-08-06 12:35:12 +0200

    added modules for database and login

--- banking_app/__init__.py
+++ banking_app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from banking_app import routes
