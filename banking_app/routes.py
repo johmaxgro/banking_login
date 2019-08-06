3a5106e5f7d24504411553a8d6e3a0bad85c427a
Johannes Groschopp <johannes.groschopp@gymder.de>
2019-08-06 12:19:54 +0200

    added basic layout html file with basic css and login file

--- banking_app/routes.py
+++ banking_app/routes.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import render_template
 from banking_app import app
 
 
 @app.route('/')
 @app.route('/login')
 def login():
-    return '<h1>Your Online Banking App!</h1>'
+    return render_template('login.html', title='Welcome to your Banking App!') 
 
 @app.route('/main')
 def main():
